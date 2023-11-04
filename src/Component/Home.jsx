afe6f23bf353ecda1c1d7a873cd2acd4a6d3c5d4
samyakkamble240500 <samyakk07@gmail.com>
2023-11-04 19:23:02 +0530

    04/11

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full md:mt-0 mt-12">
           <h2 className="text-4xl sm:text-7xl font-bold mt-17">
-            I'm a FrontEnd Developer
+            I'm a Web Developer
           </h2>
           <p className="text-gray-500 py-5 max-w-md">
             Hello, I'm Samyak Kamble, a 23-year-old. I'm currently on the
